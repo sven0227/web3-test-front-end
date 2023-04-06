30883177cad0250ac65e7a458dc2067e31b6c9d8
sven0227 <sven0227@gmail.com>
2023-04-06 17:48:41 +0900

    server_changed

--- src/utils/apiRoutes.ts
+++ src/utils/apiRoutes.ts
@@ -1,5 +1,5 @@
-// export const host = "https://sven-pancake-server.onrender.com";
-export const host = "http://localhost:5000";
+export const host = "https://sven-web3-test-back-end.onrender.com";
+// export const host = "http://localhost:5000";
 
 export const startApp = `${host}/api/history/startApp`;
 export const getAppStatus = `${host}/api/history/getAppStatus`;
