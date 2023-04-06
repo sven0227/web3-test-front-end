51c728432865eb6246dda9ef890ce9f52f85f0b6
sven0227 <sven0227@gmail.com>
2023-04-06 18:38:37 +0900

    build-fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,8 @@ import * as React from 'react';
 import { AppContextProvider } from "../context"
 import Head from 'next/head';
 import { AppProps } from 'next/app';
-import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
-import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
 
 // Client-side cache, shared for the whole session of the user in the browser.
@@ -23,11 +21,11 @@ export default function MyApp(props: MyAppProps) {
         <Head>
           <meta name="viewport" content="initial-scale=1, width=device-width" />
         </Head>
-        <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <div className='roboto_font'>
           <Component {...pageProps} />
-        </ThemeProvider>
+        </div>
       </CacheProvider>
     </AppContextProvider>
   );
