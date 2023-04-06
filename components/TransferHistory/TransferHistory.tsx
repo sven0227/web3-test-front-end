bce5dada34906ffbbe6af365f547e49c05df8305
sven0227 <sven0227@gmail.com>
2023-04-06 13:02:16 +0900

    tokenHistory-firstLoad-completed

--- components/TransferHistory/TransferHistory.tsx
+++ components/TransferHistory/TransferHistory.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -7,12 +8,14 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
+import axios from 'axios';
+import { getHistory } from '../../src/utils/apiRoutes';
 
 interface Column {
 	id: 'blockNumber' | 'from' | 'to' | 'value';
 	label: string;
 	minWidth?: number;
-	align?: 'right';
+	align?: 'right' | 'left' | 'center';
 	format?: (value: number) => string;
 }
 
@@ -23,13 +26,14 @@ const columns: readonly Column[] = [
 		id: 'to',
 		label: 'to',
 		minWidth: 170,
-		align: 'right',
+		align: 'left',
 	},
 	{
 		id: 'value',
 		label: 'value',
 		minWidth: 170,
-		align: 'right',
+		align: 'left',
+		format: (value: number) => { return (value / 1e6).toString() }
 	},
 ];
 
@@ -49,28 +53,41 @@ function createData(
 	return { blockNumber, from, to, value };
 }
 
-const rows = [
-	createData('India', 'IN', "1324171354", 3287263),
-	createData('China', 'CN', "1403500365", 9596961),
-	createData('Italy', 'IT', "60483973", 301340),
-	createData('United States', 'US', "327167434", 9833520),
-	createData('Canada', 'CA', "37602103", 9984670),
-	createData('Australia', 'AU', "25475400", 7692024),
-	createData('Germany', 'DE', "83019200", 357578),
-	createData('Ireland', 'IE', "4857000", 70273),
-	createData('Mexico', 'MX', "126577691", 1972550),
-	createData('Japan', 'JP', "126317000", 377973),
-	createData('France', 'FR', "67022000", 640679),
-	createData('United Kingdom', 'GB', "67545757", 242495),
-	createData('Russia', 'RU', "146793744", 17098246),
-	createData('Nigeria', 'NG', "200962417", 923768),
-	createData('Brazil', 'BR', "210147125", 8515767),
-];
+// const rows = [
+// 	createData('India', 'IN', "1324171354", 3287263),
+// 	createData('China', 'CN', "1403500365", 9596961),
+// 	createData('Italy', 'IT', "60483973", 301340),
+// 	createData('United States', 'US', "327167434", 9833520),
+// 	createData('Canada', 'CA', "37602103", 9984670),
+// 	createData('Australia', 'AU', "25475400", 7692024),
+// 	createData('Germany', 'DE', "83019200", 357578),
+// 	createData('Ireland', 'IE', "4857000", 70273),
+// 	createData('Mexico', 'MX', "126577691", 1972550),
+// 	createData('Japan', 'JP', "126317000", 377973),
+// 	createData('France', 'FR', "67022000", 640679),
+// 	createData('United Kingdom', 'GB', "67545757", 242495),
+// 	createData('Russia', 'RU', "146793744", 17098246),
+// 	createData('Nigeria', 'NG', "200962417", 923768),
+// 	createData('Brazil', 'BR', "210147125", 8515767),
+// ];
 
 export default function TransferHistory() {
-	const [page, setPage] = React.useState(0);
-	const [rowsPerPage, setRowsPerPage] = React.useState(10);
+	const [page, setPage] = useState(0);
+	const [rowsPerPage, setRowsPerPage] = useState(10);
+	const [rows, setRows] = useState([]);
 
+	useEffect(() => {
+		const getHistoryFunc = async () => {
+			try {
+				const { data: data } = await axios.post(getHistory, { pageNumber: 1, pageSize: 10 })
+				console.log(data);
+				setRows(data.data)
+			} catch (error) {
+
+			}
+		}
+		getHistoryFunc();
+	}, [])
 	const handleChangePage = (event: unknown, newPage: number) => {
 		setPage(newPage);
 	};
---
+++ src/utils/apiRoutes.ts
@@ -0,0 +1,7 @@
+// export const host = "https://sven-pancake-server.onrender.com";
+export const host = "http://localhost:5000";
+
+export const startApp = `${host}/api/history/startApp`;
+export const getAppStatus = `${host}/api/history/getAppStatus`;
+export const getHistory = `${host}/api/history/getHistory`;
+export const getAccounts = `${host}/api/history/getAccounts`;
